6b63b3c09bf8147c1c692f943cb954680b673aad
ricosmall <ricosmaller@gmail.com>
2024-08-07 09:04:04 +0800

    chore: delete unused vars

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Message } from "../domain/entities/Message";
 import { MarkdownRenderer } from "./MarkdownRenderer";
 
